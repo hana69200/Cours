32ce7e16b4869bb924037a48a3c1103afb46b28c
LicorneMagique <crafkiler69@gmail.com>
2018-06-12 12:01:18 +0200

    php

---
+++ S2/PHP/Seance-2/ex5.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charsert="utf-8"/>
+    <title>Impots</title>
+    <link rel="stylesheet" type="text/css" href="hello.css"/>
+  </head>
+  
+  <body>
+    <p><label>Nombre d'enfants :
+      <input type="text" name="nombreEnfant" value=<? isset($nbEnfant)/>
+    </label></p>
