7e95f1f2d088cfd6970ac75a1eb4fbc16d790ce5
LicorneMagique <crafkiler69@gmail.com>
2018-06-19 19:01:46 +0200

    php

--- S2/PHP/Cours 1/ex5.php
+++ S2/PHP/Cours 1/ex5.php
@@ -1,12 +1,33 @@
+<?php
+if (isset($_POST['nbEnfant']) && isset($_POST['salaire'])) {
+    $nbEnf = (int) $_POST['nbEnfant'];
+    $marie = isset((int) $_POST['marie'] == '1' ? true : false;
+    $salaire = (int) $_POST['salaire'];
+}
+?>
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charsert="utf-8"/>
-    <title>Impots</title>
-    <link rel="stylesheet" type="text/css" href="hello.css"/>
-  </head>
+    <head>
+        <meta charsert="utf-8"/>
+        <title>Impots</title>
+    </head>
   
-  <body>
-    <p><label>Nombre d'enfants :
-      <input type="text" name="nombreEnfant" value=<? isset($nbEnfant)/>
-    </label></p>
+    <body>
+        <form action="ex5.php" method="post">
+            <p>Nombre d'enfants : <input type="text" name="nbEnfant"></p>
+            <p>Marié : <input type="checkbox" name="marie" value="1"></p>
+            <p>Salaire annuel : <input type="text" name="salaire"></p>
+            <input type="submit" value="calculer">
+            <?php
+            if (isset($nbEnf)) {
+                echo "<p>Ça marche !</p>";
+            }
+            else {
+                echo "<p>sadness</p>";
+            }
+            ?>
+        </form>
+    </body>
+    
+</html>
