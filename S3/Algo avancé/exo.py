44e6b50dba61f6c474735636140559cc8e221977
LicorneMagique <crafkiler69@gmail.com>
2018-10-10 09:46:54 +0200

    Tentative désespérée d'EX3 en SE

---
+++ S3/Algo avancé/exo.py
@@ -0,0 +1,29 @@
+def supprDroit(tree):
+    courant = tree
+    precedent = None
+    while courant.fd != None and courant.fg != None:
+        if courant.fd == None:
+            precedent = courant
+            courant = courant.fg
+        else:
+            precedent = courant
+            courant = courant.fd
+    if precedent.fg == courant:
+        precedent.fg = None
+    else:
+        precedent.fd = None
+    free(courant)
+
+def getMirror(racine):
+    courant = racine
+    if racine != None :
+        tmp = courant.fg
+        courant.fg = courant.fd
+        courant.fd = tmp
+        
+        courant.fd = getMirror(courant.fd)
+        courant.fg = getMirror(courant.fg)
+    return courant
+
+def supprVal(racine, val):
+    
