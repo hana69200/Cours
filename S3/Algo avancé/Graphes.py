9b03697b66bc72969bae52874946d9024afd4d13
LicorneMagique <crafkiler69@gmail.com>
2018-12-13 20:50:58 +0100

    Algo fini

--- S3/Algo avancé/Graphes.py
+++
@@ -1,5 +0,0 @@
-# Algorithme de Kruskal
-
-# Trier les arêtes dans l'ordre croissant de leur poids
-# Tant que le nombre d'arête de l'arbre courant est différent de n-1,
-# Ajouter les arêtes dans l'ordre croissant dans l'arbre courant sauf si elles créent un cycle
--- S3/Algo avancé/liste_adjacence.py
+++ S3/Algo avancé/liste_adjacence.py
@@ -1,3 +1,5 @@
+import matrice as god
+
 class Arrete():
     def __init__(self, sommet1, sommet2, valuation):
         self.s1 = sommet1
@@ -7,6 +9,42 @@ class Arrete():
 
     def info(self):
         return "Arrete de",self.s1,"à",self.s2,"avec une valuation de",self.valuation
+
+def listeToMatrice(liste):
+    # Récupération de la taille du fichier
+    taille = len(liste)
+    
+    # Création de la matrice vide (tout à 0)
+    matrice = []
+    for i in range(taille):
+        matrice.append([])
+    for i in range(taille):
+        for j in range(taille):
+            matrice[i].append(0)
+    
+    # Remplissage de la matrice
+    for a in tab:
+        while a != None:
+            matrice[int(a.s1)][int(a.s2)] = int(a.valuation)
+            a = a.suiv
+    return matrice
+
+def matriceToListe(matrice):
+    tab = []
+    for i in range(len(matrice)):
+        tab.append(None)
+    for i in range(len(matrice)):
+        for j in range(len(matrice)):
+            if matrice[i][j] != 0:
+                if tab[i] == None:
+                    tab[i] = Arrete(i, j, matrice[i][j])
+                else:
+                    a = tab[i]
+                    while a.suiv != None:
+                        a = a.suiv
+                    a.suiv = Arrete(i, j, matrice[i][j])
+    return tab
+                
 
 def liste_adjacence(txt):
     tab = []
@@ -43,10 +81,17 @@ def print_liste_adjacence(tab):
             a = a.suiv
         i += 1
 
+def kruskal_liste(liste):
+    matrice = listeToMatrice(liste)
+    god.kruskal_matrice(matrice)
+    return matriceToListe(matrice)
+
 if __name__ == '__main__':
     tab = liste_adjacence("graphe.txt")
-    print_liste_adjacence(tab)
-
     
-
-
+    print("Après lecture du fichier :\n")
+    print_liste_adjacence(tab)
+    
+    print("\nAprès Kruskal :\n")
+    tab = kruskal_liste(tab)
+    print_liste_adjacence(tab)
