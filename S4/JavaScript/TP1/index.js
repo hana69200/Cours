bc57f931b09b357293a01f61a1975dea553531ae
GIRAUD JULIEN p1704709 <julien.giraud@etu.univ-lyon1.fr>
2019-02-07 19:14:06 +0100

    Ajout des modification de Jonathan en JS

--- S4/JavaScript/TP1/index.js
+++ S4/JavaScript/TP1/index.js
@@ -1,10 +1,9 @@
 // Ajout du trigger sur la page
 window.addEventListener("load", () => start());
 
-
 start = () => {
 	document.querySelector("button").disabled = true; // Désactivation du bouton
-	document.querySelectorAll("input").forEach(inp => {
+	document.querySelectorAll("input.tested").forEach(inp => {
 
 		console.log(inp);
 		const pwdSpan = document.querySelector("span#pwdPercent");
@@ -15,13 +14,19 @@ start = () => {
 			errorDiv.innerHTML = "";
 			let error = false;
 			const map = new Map();
-			document.querySelectorAll("label").forEach((l) => {
-				map[l.htmlFor] = document.querySelector(`#${l.htmlFor}`);
+			document.querySelectorAll("input.tested").forEach((l) => {
+				map[l.id] = document.querySelector(`#${l.id}.tested`);
 			});
+
+			for(let inp in map)
+			{
+				map[inp].classList.remove("error");
+			}
 
             // Vérification de l'âge
 			if (map["age"].value < 18) {
 				error = true;
+				map["age"].classList.add("error");
 				console.debug("Erreur Age");
 				const pError = document.createElement("p");
 				pError.innerText = "Vous devez avoir au moins 18 ans";
@@ -29,8 +34,9 @@ start = () => {
             }
 
             // Vérification de l'identifiant
-			if (!map["id"].value.match("^[a-zA-Z]{1,12}$")) { // Composé de 1 à 12 lettre(s)
+			if (!map["id"].value.match("^[a-zA-Z]{1,12}$")) {
 				error = true;
+				map["id"].classList.add("error");
 				console.debug("Erreur id");
 				const pError = document.createElement("p");
 				pError.innerText = "Votre identifiant doit faire entre 1 et 12 caractères et uniquement composé de lettres";
@@ -40,6 +46,7 @@ start = () => {
             // On vérifie que les mots de passes sont identitiques
 			if (!(map["pwdC"].value === map["pwd"].value)) {
 				error = true;
+				map["pwdC"].classList.add("error");
 				console.debug("Erreur Pwdc");
 				const pError = document.createElement("p");
 				pError.innerText = "Les mots de passes ne sont pas identitiques";
@@ -49,6 +56,7 @@ start = () => {
             // Vérification de la checkbox des CGU
 			if (!map["cgu"].checked) {
 				error = true;
+				map["cgu"].classList.add("error");
 				console.debug("Erreur Cgu");
 				const pError = document.createElement("p");
 				pError.innerText = "Vous devez valider les conditions générales d'utilisation";
@@ -57,11 +65,12 @@ start = () => {
 
 			const pwd = map["pwd"].value; // Mot de passe
             let pwdF = 100; // Pourcentage du mot de passe bien tapé
-            
+
             // Vérification de la taille du mot de passe
 			if (pwd.length < 8) {
 				error = true;
 				pwdF-= 20;
+				map["pwd"].classList.add("error");
 				console.debug("Erreur pwd");
 				const pPwdError = document.createElement("p");
 				pPwdError.innerText = "Le mot de passe doit faire au moins 8 caractères";
@@ -72,6 +81,7 @@ start = () => {
 			if (!pwd.match("[a-z]+")) {
 				error = true;
 				pwdF-= 20;
+				map["pwd"].classList.add("error");
 				const pPwdError = document.createElement("p");
 				pPwdError.innerText = "Le mot de passe doit au moins 1 une lettre minuscule";
 				errorDiv.appendChild(pPwdError);
@@ -81,8 +91,9 @@ start = () => {
 			if (!pwd.match("[A-Z]+")) {
 				error = true;
 				pwdF-= 20;
+				map["pwd"].classList.add("error");
 				const pPwdError = document.createElement("p");
-				pPwdError.innerText = "Le mot de passe doit avoir au moins 1 une lettre majuscule";
+				pPwdError.innerText = "Le mot de passe doit  avoir au moins 1 une lettre majuscule";
 				errorDiv.appendChild(pPwdError);
 			}
 
@@ -90,6 +101,7 @@ start = () => {
 			if (!pwd.match("[0-9]")) {
 				error = true;
 				pwdF-= 20;
+				map["pwd"].classList.add("error");
 				const pPwdError = document.createElement("p");
 				pPwdError.innerText = "Le mot de passe doit faire au moins 1 chiffre";
 				errorDiv.appendChild(pPwdError);
@@ -99,6 +111,7 @@ start = () => {
 			if (!pwd.match("[^0-9]+[^a-z]+[^A-Z]")) { // Ni chiffre ni lettre
 				error = true;
 				pwdF-= 20;
+				map["pwd"].classList.add("error");
 				const pPwdError = document.createElement("p");
 				pPwdError.innerText = "Le mot de passe doit faire au moins 1 caractère spécial";
 				errorDiv.appendChild(pPwdError);
@@ -109,17 +122,12 @@ start = () => {
 			pError.innerText = `${pwdF}%`;
 			pwdSpan.appendChild(pError);
 
+
 			console.debug("");
             console.log(map);
             
             // Gestion de l'état du bouton
-			if (!error) {
-				document.querySelector("button").disabled = false;
-			}
-			else {
-				document.querySelector("button").disabled = true;
-
-			}
+			document.querySelector("button").disabled = error;
 		});
 	});
 };
