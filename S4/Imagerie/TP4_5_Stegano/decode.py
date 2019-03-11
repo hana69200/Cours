96e9cf78bd6c2401466a72341abb6515a0236399
LicorneMagique <crafkiler69@gmail.com>
2019-03-11 09:53:29 +0100

    Imagerie décodage images

--- S4/Imagerie/TP4_5_Stegano/decode.py
+++ S4/Imagerie/TP4_5_Stegano/decode.py
@@ -1,36 +1,18 @@
 from stegano import *
-from PIL import Image
-import numpy
-import stegano
 
 
 if __name__ == "__main__":
 
     path = './exercices/'
-    filename = 'message_x.png'
-    newFilename = 'message_x_decode.png'
+    filename = 'message_x_cool.png'
+    newFilename = filename.split('.')[0] + '_decode.png'
 
-    # Read image
-    image = Image.open(path + filename)
+    size, image = image_load(path + filename)
 
-    # Read pixels
-    pixels = image.load()
+    result = image_new(size)
 
-    for x in range(size[0]):
-        for y in range(size[1]):
-            R = pixels[x, y][0]  # Couleur Rouge
-            V = pixels[x, y][1]  # Couleur Verte
-            B = pixels[x, y][2]  # Couleur Bleu
-            pixels[x, y] = (R, V, B)
+    for x in range(size[0] * size[1] * 3):
+        pixel = image[x]
+        result[x] = low(pixel, 4) << 4
 
-    for x in range(size[0]):
-        for y in range(size[1]):
-            R = pixels[x, y][0]
-            V = pixels[x, y][1]
-            B = pixels[x, y][2]
-            G = int(0.299 * R + 0.587 * V + 0.114 * B)# coding: utf8
-
-            pixels[x, y] = (G, G, G)
-
-    # Enregistre l'image
-    image.save(path + newFilename, "PNG")
+    image_save(result, size, path + newFilename)
